09d29fc5214978856646ad3f5f2e0d5ddd9a31c3
Gábor Librecz <kuglee@gmail.com>
2024-06-05 16:14:25 +0200

    Add NavigationStack

--- NavigationExperiments/ContentView.swift
+++ NavigationExperiments/ContentView.swift
@@ -8,11 +8,21 @@ import SwiftUI
     case featureC(FeatureC)
   }
 
-  @ObservableState struct State { @Presents var detail: Detail.State? }
+  @Reducer(state: .equatable, .sendable, action: .sendable) public enum Path {
+    case featureA(FeatureA)
+    case featureB(FeatureB)
+    case featureC(FeatureC)
+  }
+
+  @ObservableState struct State {
+    @Presents var detail: Detail.State?
+    var path = StackState<Path.State>()
+  }
 
   enum Action: BindableAction {
     case binding(BindingAction<State>)
     case detail(PresentationAction<Detail.Action>)
+    case path(StackActionOf<Path>)
   }
 
   var body: some ReducerOf<Self> {
@@ -22,9 +32,10 @@ import SwiftUI
       switch action {
       case .binding: return .none
       case .detail: return .none
+      case .path: return .none
       }
     }
-    .ifLet(\.$detail, action: \.detail) { Detail.body }
+    .ifLet(\.$detail, action: \.detail) { Detail.body }.forEach(\.path, action: \.path)
   }
 }
 
@@ -32,8 +43,25 @@ public struct AppView: View {
   @Bindable var store: StoreOf<AppFeature>
 
   @State var columnVisibility = NavigationSplitViewVisibility.all
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
 
   public var body: some View {
+    if self.horizontalSizeClass == .compact {
+      self.navigationStack
+    } else {
+      self.navigationSplitView
+    }
+  }
+
+  var navigationStack: some View {
+    NavigationStack(path: self.$store.scope(state: \.path, action: \.path)) {
+      self.rootView(store: store.scope(state: \.detail, action: \.detail.presented))
+    } destination: {
+      self.destinationView(store: $0)
+    }
+  }
+
+  var navigationSplitView: some View {
     NavigationSplitView(columnVisibility: self.$columnVisibility) {
       List(selection: $store.detail) {
         NavigationLink(value: AppFeature.Detail.State.featureA(FeatureA.State())) {
@@ -47,22 +75,62 @@ public struct AppView: View {
         }
       }
     } detail: {
-      if let store = store.scope(state: \.detail, action: \.detail.presented) {
-        switch store.case {
-        case let .featureA(store): FeatureAView(store: store)
-        case let .featureB(store): FeatureBView(store: store)
-        case let .featureC(store): FeatureCView(store: store)
-        }
+      NavigationStack(path: self.$store.scope(state: \.path, action: \.path)) {
+        self.rootView(store: store.scope(state: \.detail, action: \.detail.presented))
+      } destination: {
+        self.destinationView(store: $0)
       }
     }
     .navigationSplitViewStyle(.balanced)
+  }
+
+  @ViewBuilder func rootView(store: Store<AppFeature.Detail.State, AppFeature.Detail.Action>?)
+    -> some View
+  {
+    if let store {
+      switch store.case {
+      case let .featureA(store): FeatureAView(store: store)
+      case let .featureB(store): FeatureBView(store: store)
+      case let .featureC(store): FeatureCView(store: store)
+      }
+    } else {
+      Text("root")
+    }
+  }
+
+  @ViewBuilder func destinationView(store: Store<AppFeature.Path.State, AppFeature.Path.Action>)
+    -> some View
+  {
+    switch store.case {
+    case let .featureA(store): FeatureAView(store: store)
+    case let .featureB(store): FeatureBView(store: store)
+    case let .featureC(store): FeatureCView(store: store)
+    }
   }
 }
 
 #Preview { AppView(store: Store(initialState: .init(), reducer: { AppFeature()._printChanges() })) }
 
 @Reducer public struct FeatureA {
-  @ObservableState public struct State: Hashable { var title = "Feature A" }
+  @ObservableState public struct State: Hashable {
+    let title = "Feature A"
+    var count: Int
+
+    init(count: Int = 0) { self.count = count }
+  }
+
+  public enum Action: Sendable { case count }
+
+  public var body: some Reducer<State, Action> {
+    Reduce { state, action in
+      switch action {
+      case .count:
+        state.count += 1
+
+        return .none
+      }
+    }
+  }
 }
 struct FeatureAView: View {
   let store: StoreOf<FeatureA>
@@ -71,6 +139,7 @@ struct FeatureAView: View {
     ScrollView {
       VStack {
         Text(store.title)
+        Button("Count: \(self.store.count)") { self.store.send(.count) }
         Rectangle().fill(.red).frame(height: 2000, alignment: .top)
       }
     }
@@ -78,7 +147,7 @@ struct FeatureAView: View {
 }
 
 @Reducer public struct FeatureB {
-  @ObservableState public struct State: Hashable { var title = "Feature B" }
+  @ObservableState public struct State: Hashable { let title = "Feature B" }
 }
 struct FeatureBView: View {
   let store: StoreOf<FeatureB>
@@ -94,7 +163,7 @@ struct FeatureBView: View {
 }
 
 @Reducer public struct FeatureC {
-  @ObservableState public struct State: Hashable { var title = "Feature C" }
+  @ObservableState public struct State: Hashable { let title = "Feature C" }
 }
 struct FeatureCView: View {
   let store: StoreOf<FeatureC>
