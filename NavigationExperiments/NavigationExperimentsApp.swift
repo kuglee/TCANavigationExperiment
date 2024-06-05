0fbf7dabee046b6f0e88b480cd24c3aca02e2999
Gábor Librecz <kuglee@gmail.com>
2024-06-05 14:03:30 +0200

    Initial Commit

---
+++ NavigationExperiments/NavigationExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExperimentsApp.swift
+//  NavigationExperiments
+//
+//  Created by Librecz Gábor on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
